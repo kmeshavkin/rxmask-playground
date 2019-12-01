cb334f97347674930919e920813bea064b500d38
Kostyan1996 <meshavkin1996@gmail.com>
2019-12-01 23:46:02 +0300

    Simple mask

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,54 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { TextField, Typography } from '@material-ui/core';
+import { Wrapper } from './App.styled';
+import rxmask from 'rxmask';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export interface AppProps {
+  
 }
+ 
+export interface AppState {
+  example: {
+    ref: React.RefObject<HTMLTextAreaElement>,
+    parser: rxmask | null
+  }
+}
+ 
+class App extends React.Component<AppProps, AppState> {
+  constructor(props: AppProps) {
+    super(props);
 
+    const exampleRef = React.createRef() as React.RefObject<HTMLTextAreaElement>;
+
+    this.state = {
+      example: {
+        ref: exampleRef,
+        parser: null
+      }
+    };
+  }
+
+  componentDidMount() {
+    const { example } = this.state;
+    this.setState({
+      example: {
+        ...example,
+        parser: new rxmask({ mask: '***-**-**' }, example.ref.current)
+      }
+    });
+  }
+
+  render() {
+    const { example } = this.state;
+
+    return (
+      <Wrapper>
+        <Typography variant="h2">rxmask.js</Typography>
+        <Typography variant="h6">Advanced mask parser for html input or raw string parsing</Typography>
+        <TextField inputRef={example.ref} onChange={() => example.parser!.onInput()} />
+      </Wrapper>
+    );
+  }
+}
+ 
 export default App;
